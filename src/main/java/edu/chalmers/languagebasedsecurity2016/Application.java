d01e227eb30c72856087b06f9824c192cda7802d
Peter Eliasson <warackta@gmail.com>
2016-03-20 16:48:12 +0100

    Initial version.

---
+++ src/main/java/edu/chalmers/languagebasedsecurity2016/Application.java
@@ -0,0 +1,69 @@
+package edu.chalmers.languagebasedsecurity2016;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Application {
+
+    private final Pocket pocket;
+    private final Wallet wallet;
+    private final BufferedReader reader;
+
+    public Application() throws Exception {
+        wallet = new Wallet();
+        pocket = new Pocket();
+        reader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public void run() throws Exception {
+        printBalance();
+        printProductList();
+        promptProductPurchase();
+        printBalance();
+    }
+
+    private void printBalance() throws IOException {
+        System.out.printf("Your balance: %d credits\n", wallet.getBalance());
+    }
+
+    private void printProductList() {
+        System.out.println(Store.asString());
+    }
+
+    private void promptProductPurchase() throws Exception {
+        String productName;
+        do {
+            System.out.print("What you want to buy?: ");
+            productName = reader.readLine();
+            if (productName.isEmpty()) {
+                System.exit(0);
+            }
+        } while (!Store.products.containsKey(productName));
+
+        purchaseProduct(productName);
+    }
+
+    private void purchaseProduct(String productName) throws Exception {
+        int cost = Store.products.get(productName);
+        int balance = wallet.getBalance();
+        // check if the amount of credits is enough, if not stop the execution.
+        if (balance < cost) {
+            System.exit(-1);
+        }
+        // otherwise, withdraw the price of the product from the wallet.
+        wallet.setBalance(balance - cost);
+        // add the name of the product to the pocket file.
+        pocket.addProduct(productName);
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            new Application().run();
+        } catch (Exception e) {
+            System.err.println("Could not start application!");
+            e.printStackTrace();
+        }
+    }
+}
